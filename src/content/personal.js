745866c7ee6b2ee01d625d85fc46c5dd395c465d
Nikhil Ravi <nr337@cornell.edu>
2023-10-13 21:23:03 -0400

    Update personal.js

--- src/content/personal.js
+++ src/content/personal.js
@@ -4,6 +4,6 @@ module.exports = [
     familyName: "Ravi",
     givenName: "Some",
     title: "Graduate Research Assistant",
-    body: "I am a Doctoral student with research experience in complex networked dynamic systems, with a focus on energy systems. Advanced understanding of linear algebra, statistics, stochastic processes, optimization, and probability theory. Skilled in time-series predictive modeling, causal inference, and hypothetical testing. Looking for full-time data science/quantitative research roles starting May 2023.",
+    body: "I am a Doctoral student with research experience in complex networked dynamic systems, with a focus on energy systems. Advanced understanding of linear algebra, statistics, stochastic processes, optimization, and probability theory. Skilled in time-series predictive modeling, causal inference, and hypothetical testing. Looking for full-time data science/quantitative research roles starting December 2023.",
   },
 ];
